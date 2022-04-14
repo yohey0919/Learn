d2a74b24409b7a4a2011e4a21033a20a7c95ce58
yohey0919 <1472408689@qq.com>
2022-04-14 10:39:36 +0800

    update hello.go

--- learn/code1/hello.go
+++ learn/code1/hello.go
@@ -6,6 +6,6 @@ import "fmt" //fmt包，就含有格式化输出、接收输入的函数
 
 // 在main里的main 函数 也很特殊，它是整个程序执行时的入口
 func main() { // 1)fun必须与{ 同一行  , 2)一个go.mod里面只能有一个main()方法
-	fmt.Println("Hello, World! -- 1")
-	fmt.Println("Hello, World! -- 2") // 格式相关： 1)换行符 <=> 分号（;） 2)代码保存会“自动格式化”为标准格式
+	fmt.Println("Hello, World! -- 11")
+	fmt.Println("Hello, World! -- 22") // 格式相关： 1)换行符 <=> 分号（;） 2)代码保存会“自动格式化”为标准格式
 }
